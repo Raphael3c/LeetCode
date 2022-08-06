f9a3f0b45b9dee9335206ad4f5d2ea2d492f9220
Rapha <phael.ufc@gmail.com>
2022-08-05 23:57:09 -0300

    821. Shortest Distance to a Character

---
+++ 821. Shortest Distance to a Character/src.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {character} c
+ * @return {number[]}
+ */
+ var shortestToChar = function(s, c) {
+
+    const array = new Array(s.length)
+    let initialPosition = 0;
+    let endPosition = 0;
+    let aux = 0;
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === c){
+            endPosition = i
+            aux = 0
+            for(let j = endPosition; j >= initialPosition; j--){
+                array[j] = aux;
+                aux = aux + 1;
+            }
+            const quantityElements = initialPosition - endPosition
+            if(quantityElements > 1){
+                const count = Math.floor(quantityElements/2)
+                aux = 1
+                for(let j = initialPosition; j < count+initialPosition; j++){
+                    array[j] = aux;
+                    aux = aux + 1;
+                }
+            }
+            initialPosition = endPosition+1;
+        }
+    }
+    return array
+};
+
+console.log(shortestToChar("loveleetcode", "e"))
