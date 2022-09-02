637bced0518667ae51939384fca7367ed34c5d00
Rapha <phael.ufc@gmail.com>
2022-09-02 00:16:57 -0300

    202. Almost there

--- 202. Happy Number/src.js
+++ 202. Happy Number/src.js
@@ -4,31 +4,33 @@
  */
 
  var isHappy = function(n) {
-    if(!(1 <= n <= Math.pow(2, 31) - 1))
+    const constraint = (1 <= n <= Math.pow(2, 31) - 1);
+    if(!constraint)
         return
 
     let result = 0;
-    let number = n;
-
+    let helper = n;
+    
     while(1){
-
-        if(result == n*n)
-            return false
-
-        const numberSplitted = number.toString().split("");
+        console.log(helper)
+        const helperSplitted = helper.toString().split("");
         result = 0;
 
-        for(let i = 0 ; i < numberSplitted.length ; i++){
-            result = result + Math.pow(Number(numberSplitted[i]), 2);
+        for(let i = 0 ; i < helperSplitted.length ; i++){
+            result = result + Math.pow(Number(helperSplitted[i]), 2);
         }
 
         if(result == 1)
             break
 
-        number = result;
+        if(helper%10 == 0)
+            return false
+
+        helper = result;
     }
 
     return true
 };
+
 
 console.log(isHappy(7))
